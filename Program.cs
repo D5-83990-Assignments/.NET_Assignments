f92342dd712b7d7d550a060d4d136aa3f3c26c33
D5_83990_Abhishek <162444911+Abhishek-Bhopale@users.noreply.github.com>
2024-05-27 14:28:10 +0530

    Question 7

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
-﻿namespace Que_06
+﻿using System.Net.Cache;
+
+namespace Que_07
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Date d1 = new Date(10,6,2001);
-            Console.WriteLine(d1.ToString());
-            Console.WriteLine(d1.IsValid());
+            Person person = new Person();
+            person.Accept();
+            person.Print();
             Console.ReadLine();
-
-
+ 
         }
     }
 
@@ -19,7 +20,9 @@
         private int _Month;
         private int _Year;
 
-        public Date(int Day,int Month,int Year)
+        public Date() {    }
+
+        public Date(int Day, int Month, int Year)
         {
             _Day = Day;
             _Month = Month;
@@ -58,12 +61,12 @@
 
         public void PrintDate()
         {
-            Console.WriteLine("Date :- "+_Day + "/" + _Month + "/" + _Year);
+            Console.WriteLine("Date :- " + _Day + "/" + _Month + "/" + _Year);
         }
 
         public bool IsValid()
         {
-            if((_Day > 1 && _Day<=31) && (_Month > 1 && _Month <= 12) && (_Year > 1 && _Year <= 9999) )
+            if ((_Day > 1 && _Day <= 31) && (_Month > 1 && _Month <= 12) && (_Year > 1 && _Year <= 9999))
             {
                 return true;
             }
@@ -73,7 +76,85 @@
 
         public string ToString()
         {
-            return _Day+"/"+_Month+"/"+_Year;
+            return _Day + "/" + _Month + "/" + _Year;
+        }
+
+    }
+
+    class Person
+    {
+        private string? _Name;
+        private string? _Address;
+        private bool _Gender;
+        private Date? date;
+
+        public Person() 
+        {  
+            date = new Date();
+        }
+
+        public Person(string name,string address,bool gender)
+        {
+            _Name = name;
+            _Address = address;
+            _Gender = gender;
+            date = new Date();
+            date.AcceptDate();
+        }
+
+        public void Accept()
+        {
+            Console.Write("Enter the name :- ");
+            _Name = Console.ReadLine();
+            Console.Write("Enter the address :- ");
+            _Address = Console.ReadLine();
+            Console.Write("Enter the gender:- ");
+            _Gender = Convert.ToBoolean(Console.ReadLine());
+            date.AcceptDate();
+        }
+
+        public Date? Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+
+        public bool Gender
+        {
+            get { return _Gender; }
+            set { _Gender = value; }
+        }
+
+
+        public string? Address
+        {
+            get { return _Address; }
+            set { _Address = value; }
+        }
+
+
+        public string? Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public void Print()
+        {
+            DateTime tod = DateTime.Today;
+            Console.WriteLine("Name :- " + _Name);
+            Console.WriteLine("Address :- " + _Address);
+            Console.WriteLine("Gender :- " + _Gender);
+            Console.WriteLine("Date of Birth :- " + date.ToString());
+            if(date.Month <= Convert.ToInt32(tod.ToString("MM")))
+            {
+                Console.WriteLine("Age :- " + (Convert.ToInt32(tod.ToString("yyyy")) - date.Year));
+            }
+            else
+            {
+                Console.WriteLine("Age :- " + ((Convert.ToInt32(tod.ToString("yyyy")) - 1) - date.Year));
+            }
         }
 
     }
